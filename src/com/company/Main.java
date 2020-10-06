36481bc57276c83aa1fad5105951d40618905c1c
LeShawn Edouard <leshawnmedouard@gmail.com>
2020-10-06 10:52:32 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte byteValue = 120;
+        short shortValue = 32750;
+        int intValue = 500;
+        long longTotal = 50000L + 10L * (byteValue + shortValue + intValue);
+        System.out.println(longTotal); // No casting needed because Java assumes the above variables are int
+
+        short shortTotal = (short) (1000 + 10 * (byteValue + shortValue + intValue));
+        System.out.println(shortTotal);
+    }
+}
